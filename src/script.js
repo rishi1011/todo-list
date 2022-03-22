b0fca266f7cfbb6c15a30753c5b9ab6c2a9b853c
rishikrishna <r.rishikrishna@gmail.com>
2022-03-22 22:40:29 +0530

    Improved UI

--- src/script.js
+++ src/script.js
@@ -1,36 +1,81 @@
 import { format } from 'date-fns';
 import inLocale  from 'date-fns/locale/en-IN';
 
+import TodoTask from './todo.js';
+
 const newTaskContainer = document.querySelector('.new-task');
 const newTaskTitle = document.querySelector('.task-title');
+const newTaskDesc = document.querySelector('.task-desc');
+const newTaskSchedule = document.querySelector('.task-schedule');
+const newTaskProject = document.querySelector('.select-project');
 const addTaskBtn = document.querySelector('.add-task');
 const cancelBtn = document.querySelector('.new-task > .btn.cancel');
 const addBtn = document.querySelector('.new-task > .btn.add');
 const taskSchedule = document.querySelector('.task-schedule');
-
+const menu = document.querySelector('.material-icons-outlined.menu-icon');
+const sidebar = document.querySelector('.sidebar');
+const checkboxes = document.querySelectorAll('.checkbox');
 
 let today = format(new Date(), 'yyyy-MM-dd',
     { locale: inLocale },
 );
 taskSchedule.value = today;
 
-addBtn.addEventListener('click', () => {
-    if (newTaskTitle.value === "") {
-        alert('Task title is required!');
-    }
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', () => {
+        if (checkbox.innerText === 'check_box') {
+            checkbox.innerText = 'check_box_outline_blank';
+        } else {
+            checkbox.innerText = 'check_box';
+        }
 
-    
+        let task = checkbox.parentElement;
+        task.classList.toggle('strike');
+
+        let p = checkbox.nextElementSibling;
+        // if (!p.innerText.contains('<strike>')) {
+        //     p.innerHTML = `<strike>${p.innerText}</strike>`
+        // } else {
+        //     p.innerHTML = `<strike>${p.innerText}</strike>`
+        // }
+    })
+})
+
+
+menu.addEventListener('click', () => {
+    sidebar.classList.toggle('hide');
 });
 
 
+addBtn.addEventListener('click', () => {
+    if (newTaskTitle.value === '') {
+        alert('Task title is required!');
+        return;
+    }
+
+    console.log(newTaskTitle.value, newTaskDesc.value, newTaskSchedule.value, newTaskProject.value);
+    
+    let newTodoTask = new TodoTask(newTaskTitle.value, newTaskDesc.value, newTaskSchedule.value, newTaskProject.value);
+    
+    console.log(newTaskTitle.value, newTaskDesc.value, newTaskSchedule.value, newTaskProject.value);
+});
+
 addTaskBtn.addEventListener('click', () => {
-    newTaskContainer.classList.remove('hide');
-    addTaskBtn.classList.add('hide');
+    showNewTaskContainer();
 });
 
 cancelBtn.addEventListener('click', () => {
+    hideNewTaskContainer();
+});
+
+function showNewTaskContainer() {
+    newTaskContainer.classList.remove('hide');
+    addTaskBtn.classList.add('hide');
+}
+
+function hideNewTaskContainer() {
     newTaskContainer.classList.add('hide');
     addTaskBtn.classList.remove('hide');
-})
+}
 
 
--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,47 @@
-class Todo{
+export default class Todo{
     constructor(title, desc, date, project) {
         this.title = title;
         this.desc = desc;
         this.date = date;
         this.project = project;
-    }    
+    }
+    
+    get title() {
+        return this.title;
+    }
+
+    get desc() {
+        return this.desc;
+    }
+
+    get date() {
+        return this.date;
+    }
+
+    get project() {
+        return this.project;
+    }
+
+    set title(title) {
+        if (title === '') {
+            alert('Title is required!');
+            return;
+        }
+
+        this._title = title;
+    }
+
+    set desc(desc) {
+        this._desc = desc;
+    }
+
+    set date(date) {
+        this._date = date;
+    }
+
+    set project(project) {
+        this._project = project;
+    }
+
+
 }
