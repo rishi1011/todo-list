de29736e3676d09c803c207670155a7a42875143
rishikrishna <r.rishikrishna@gmail.com>
2022-04-04 21:55:41 +0530

    bug fix

--- src/storage.js
+++ src/storage.js
@@ -2,12 +2,14 @@ export default class Storage{
     static key = 'todolist';
 
     static storeContent(allProjects) {
-        console.log('hi');
         localStorage.setItem('todolist', JSON.stringify(allProjects));
     }
     
     static getContent() {
         const allProjects = JSON.parse(localStorage.getItem('todolist'));
+        allProjects.forEach(project => {
+
+        });
         return allProjects;
     }
 }
